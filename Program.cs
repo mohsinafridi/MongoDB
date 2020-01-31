511e9c40c7b51f601634e054c09cb509f2b07752
moaz <mohsin.afridi91@gmail.com>
2020-01-31 15:40:10 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace MongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("employeeDB");
+            var collection = database.GetCollection<BsonDocument>("employee");
+
+
+            var data = new BsonDocument
+            {
+                { "name" , "Mohsin Azam" },
+                {"age" , "28" },
+                {"email" , "mohsin.azam@gmail.com" },
+                {"position" , "Developer" }
+            };
+            collection.InsertOneAsync(data);
+            Console.ReadKey();
+        }
+    }
+}
